6096b5552b876cb92070f3b03ad52d92ddb585f0
reubenjohn <reubenvjohn@gmail.com>
2014-05-27 16:18:22 +0300

    close #17 + glass theme + filler->vertical progress

--- src/com/reubenjohn/studytimer/LapsFragment.java
+++ src/com/reubenjohn/studytimer/LapsFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.reubenjohn.studytimer.data.LapsCursorAdapter;
 import com.reubenjohn.studytimer.data.StudyTimerDBManager;
@@ -16,18 +17,32 @@ public class LapsFragment extends Fragment {
 	private LapsCursorAdapter adapter;
 	private StudyTimerDBManager db;
 	ListView listView;
+	TextView currentLap;
 	String formatedAverage = null;
 	long average = 0;
+	int cached_lapCount;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.laps_fragment, container, false);
-		listView = (ListView) v.findViewById(R.id.lv_laps);
 		db = new StudyTimerDBManager(getActivity().getApplicationContext());
 		db.open();
+		bridgeXML(v);
+		initalizeFeilds();
 		generateListView();
 		return v;
+	}
+
+	protected void bridgeXML(View v) {
+		listView = (ListView) v.findViewById(R.id.lv_laps);
+		currentLap = (TextView) v.findViewById(R.id.tv_current_lap);
+	}
+
+	protected void initalizeFeilds() {
+		assert currentLap != null;
+		cached_lapCount = getLapCount();
+		updateCurentLap(cached_lapCount);
 	}
 
 	protected void generateListView() {
@@ -52,11 +67,23 @@ public class LapsFragment extends Fragment {
 	public void addlap(String newLap, int newElapsed) {
 		db.addLap(newLap, newElapsed);
 		generateListView();
+		cached_lapCount++;
+		updateCurentLap(cached_lapCount);
 		// average=getAverage();
+
+	}
+
+	protected void updateCurentLap(int lapCount) {
+		if (currentLap != null)
+			currentLap.setText(Integer.toString(lapCount + 1));
 	}
 
 	public int getLapCount() {
-		return db.getLapCount();
+		assert db != null;
+		if (db == null) {
+			return -1;
+		} else
+			return db.getLapCount();
 	}
 
 	@Override
