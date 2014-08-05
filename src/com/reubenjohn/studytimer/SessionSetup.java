bed7f427cdd656d7cbfd686c4c9bd8c57bc3ea0d
Reuben John <reubenvjohn@gmail.com>
2014-08-05 17:18:32 +0530

    added total time feature to Create session activity + using tab system(currently hidden by action bar) instead of ScrollView

--- src/com/reubenjohn/studytimer/SessionSetup.java
+++ src/com/reubenjohn/studytimer/SessionSetup.java
@@ -1,8 +1,14 @@
 package com.reubenjohn.studytimer;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.view.ActionMode;
 import android.util.Log;
@@ -10,13 +16,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.TimePicker.OnTimeChangedListener;
 
+import com.reubenjohn.studytimer.R.integer;
+import com.reubenjohn.studytimer.StudyTimer.defaults;
 import com.reubenjohn.studytimer.preferences.STSP;
 import com.reubenjohn.studytimer.timming.Time;
 import com.reubenjohn.studytimer.util.kankan.widget.OnWheelChangedListener;
@@ -24,10 +34,10 @@ import com.reubenjohn.studytimer.util.kankan.widget.WheelView;
 import com.reubenjohn.studytimer.util.kankan.widget.adapters.NumericWheelAdapter;
 
 public class SessionSetup extends ActionBarActivity implements
-		OnCheckedChangeListener, OnWheelChangedListener {
+		OnCheckedChangeListener, OnWheelChangedListener, OnTimeChangedListener {
 
 	CheckBox smartTargetTimeToggle;
-	TimePicker targetPicker;
+	TimePicker targetPicker, totalTimePicker;
 	TextView smartTargetTimeToggleDescription, targetTimeTitle,
 			targetTimeDescription;
 	WheelView wheelView_100, wheelView_10, wheelView_1;
@@ -67,8 +77,9 @@ public class SessionSetup extends ActionBarActivity implements
 					targetPicker.getCurrentMinute(), 0);
 			sessionInfo.putLong(STSP.keys.targetTime, target);
 			sessionInfo.putInt(STSP.keys.totalLaps, getTotalLaps());
-			Log.d("StudyTimerSessionSetup", "Bundled new sessionInfo->{" + STSP.keys.targetTime + ":"
-					+ target + "," + "TotalLaps" + ":" + getTotalLaps() + "}");
+			Log.d("StudyTimerSessionSetup", "Bundled new sessionInfo->{"
+					+ STSP.keys.targetTime + ":" + target + "," + "TotalLaps"
+					+ ":" + getTotalLaps() + "}");
 			result.putExtras(sessionInfo);
 			setResult(RESULT_OK, result);
 			finish();
@@ -110,8 +121,9 @@ public class SessionSetup extends ActionBarActivity implements
 	private void bridgeXML() {
 		smartTargetTimeToggle = (CheckBox) findViewById(R.id.cb_smart_target_time);
 		targetPicker = (TimePicker) findViewById(R.id.tp_target_time);
-		targetTimeTitle = (TextView) findViewById(R.id.tv_session_setup_target_time_title);
-		targetTimeDescription = (TextView) findViewById(R.id.tv_session_setup_target_time_description);
+		totalTimePicker = (TimePicker) findViewById(R.id.tp_total_time);
+		targetTimeTitle = (TextView) findViewById(R.id.tv_session_create_target_time_title);
+		targetTimeDescription = (TextView) findViewById(R.id.tv_session_create_target_time_description);
 		smartTargetTimeToggleDescription = (TextView) findViewById(R.id.tv_smart_target_time_description);
 		wheelView_1 = (WheelView) findViewById(R.id.wv_total_lap_picker_digit1);
 		wheelView_10 = (WheelView) findViewById(R.id.wv_total_lap_picker_digit10);
@@ -121,23 +133,26 @@ public class SessionSetup extends ActionBarActivity implements
 
 	private void initializeFeilds() {
 		smartTargetTimeToggle.setOnCheckedChangeListener(this);
-		long targerTime = getSharedPreferences(STSP.fileNames.currentSession,
-				Context.MODE_PRIVATE).getLong(STSP.keys.targetTime,
+		SharedPreferences sessionPrefs = getSharedPreferences(
+				STSP.fileNames.currentSession, Context.MODE_PRIVATE);
+		long targerTime = sessionPrefs.getLong(STSP.keys.targetTime,
 				StudyTimer.defaults.targetTime);
+		int totalLaps = sessionPrefs.getInt(STSP.keys.totalLaps,
+				defaults.totalLaps);
+		Log.d("SessionSetup", "Retreived targetTime=" + targerTime);
+		Log.d("SessionSetup", "Retreived totalLaps=" + totalLaps);
+
 		Time currentTargetTime = new Time(targerTime);
-		Log.d("SessionSetup", "Retrerived targetTime=" + targerTime);
 		targetPicker.setCurrentHour((int) currentTargetTime.getMinutes());
 		targetPicker.setCurrentMinute((int) currentTargetTime.getSeconds());
+
+		setTotalSessionTimePicker(targerTime,
+				sessionPrefs.getInt(STSP.keys.totalLaps, defaults.totalLaps));
+
 		targetPicker.setIs24HourView(true);
-		targetPicker
-				.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-
-					@Override
-					public void onTimeChanged(TimePicker view, int hourOfDay,
-							int minute) {
-
-					}
-				});
+		targetPicker.setOnTimeChangedListener(this);
+		totalTimePicker.setIs24HourView(true);
+		totalTimePicker.setOnTimeChangedListener(this);
 
 		sessionInfo = new Bundle();
 
@@ -149,21 +164,16 @@ public class SessionSetup extends ActionBarActivity implements
 		initializeDigitWheel(R.id.wv_total_lap_picker_digit10);
 		initializeDigitWheel(R.id.wv_total_lap_picker_digit100);
 
-		if (getTotalLaps() < 100)
-			wheelView_100.setVisibility(View.GONE);
-		if (getTotalLaps() < 10)
-			wheelView_10.setVisibility(View.GONE);
+		setTotalLaps(totalLaps);
 	}
 
 	@Override
 	protected void onPause() {
-		// TODO Auto-generated method stub
 		super.onPause();
 	}
 
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 	}
 
@@ -262,4 +272,64 @@ public class SessionSetup extends ActionBarActivity implements
 		return digit100 * 100 + digit10 * 10 + digit1;
 	}
 
+	private void setTotalLaps(int totalLaps) {
+		if (totalLaps >= 10)
+			wheelView_10.setVisibility(View.VISIBLE);
+		else
+			wheelView_10.setVisibility(View.GONE);
+		if (totalLaps >= 100)
+			wheelView_100.setVisibility(View.VISIBLE);
+		else
+			wheelView_100.setVisibility(View.GONE);
+		
+		wheelView_1.setCurrentItem(totalLaps % 10);
+		totalLaps /= 10;
+		wheelView_10.setCurrentItem(totalLaps % 10);
+		totalLaps /= 10;
+		wheelView_100.setCurrentItem(totalLaps % 10);
+		totalLaps /= 10;
+	}
+
+	@Override
+	public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+		switch (view.getId()) {
+		case R.id.tp_target_time:
+			if (hourOfDay == 0 && minute == 0) {
+				view.setCurrentMinute(1);
+				// TODO flash the Target time text in red and then fade it away
+				targetTimeTitle.setTextColor(Color.RED);
+				new Handler().postDelayed(new Runnable() {
+
+					@Override
+					public void run() {
+						targetTimeTitle.setTextColor(Color.BLACK);
+
+					}
+				}, 1000);
+			} else {
+				setTotalSessionTimePicker(
+						Time.getTimeInMilliseconds(0, 0, hourOfDay, minute, 0),
+						getTotalLaps());
+			}
+			break;
+		case R.id.tp_total_time:
+			setTargetTimePicker(Time.getTimeInMilliseconds(0, hourOfDay,
+					minute, 0, 0));
+			break;
+		}
+	}
+
+	protected void setTargetTimePicker(long totalSessionTimeInMilliSeconds) {
+		Time targetTime = new Time(totalSessionTimeInMilliSeconds
+				/ getTotalLaps());
+		targetPicker.setCurrentHour((int) targetTime.getMinutes());
+		targetPicker.setCurrentMinute((int) targetTime.getSeconds());
+	}
+
+	protected void setTotalSessionTimePicker(long targetTime, int totalLaps) {
+		long totalTime = targetTime * totalLaps;
+		Time totalSessionTime = new Time(totalTime);
+		totalTimePicker.setCurrentHour((int) totalSessionTime.getHours());
+		totalTimePicker.setCurrentMinute((int) totalSessionTime.getMinutes());
+	}
 }
